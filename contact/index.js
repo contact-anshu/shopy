9ad1791ba83dd322db8302aea281d230d8172c48
contact-anshu <135035538+contact-anshu@users.noreply.github.com>
2024-07-30 13:36:35 +0530

    Add files via upload

---
+++ contact/index.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".text_btn")[0].addEventListener("click",function()
+{
+    alert("Form submit successfully");
+})
---
+++ index.js
@@ -0,0 +1,15 @@
+for(var i=0;i<6;i++)
+{
+document.querySelectorAll(".btn1")[i].addEventListener("click",function()
+{
+    alert("Sucessfully buyed");
+})
+}
+
+for(var i=0;i<6;i++)
+    {
+    document.querySelectorAll(".btn2")[i].addEventListener("click",function()
+    {
+        alert("Item added to Cart");
+    })
+    }
